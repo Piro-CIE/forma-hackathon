3e942005fc4c219c4fbb876da16a18993b64ad36
AlexPiro <alexandre@piro-cie.fr>
2024-02-14 08:37:32 +0100

    add websocket on server add send geometry function ovre websocket

--- forma-extension/server.js
+++ forma-extension/server.js
@@ -1,8 +1,11 @@
 import express from 'express'
+import cors from 'cors'
+import { WebSocketServer } from 'ws';
 
 const PORT = 3006;
 
 const app = express()
+app.use(cors())
 
 app.use(express.static('www'))
 
@@ -14,3 +17,25 @@ const server = app.listen(PORT, ()=>{
     console.log(`Server listening on port ${PORT} ...`)
 })
 
+const wss = new WebSocketServer({ server });
+
+wss.on('connection', function(ws) {
+  console.log("client joined.");
+
+  ws.on('message', function(data) {
+    if (typeof(data) === "string") {
+      // client sent a string
+      // console.log("string received from client -> '" + data + "'");
+      wss.clients.forEach(client => client.send(data))
+
+    } else {
+      // console.log("binary received from client -> " + Array.from(data).join(", ") + "");
+    //   console.log('binary received')
+      wss.clients.forEach(client => client.send(data))
+    }
+  });
+
+  ws.on('close', function() {
+    console.log("client left.");
+  });
+});
--- forma-extension/www/forma.js
+++ forma-extension/www/forma.js
@@ -1,6 +1,32 @@
-import {Forma} from "https://esm.sh/forma-embedded-view-sdk/auto";
+import { Forma } from "https://esm.sh/forma-embedded-view-sdk/auto";
 
-const element = document.createElement("div");
-element.innerHTML = `Welcome to ${Forma.getProjectId()}`;
-document.body.appendChild(element);
-window.Forma = Forma;
+// const element = document.createElement("div");
+// element.innerHTML = `Welcome to ${Forma.getProjectId()}`;
+// document.body.appendChild(element);
+// window.Forma = Forma;
+
+
+// Create WebSocket connection.
+const socket = new WebSocket("ws://localhost:3006");
+
+// // Connection opened
+// socket.addEventListener("open", (event) => {
+//   socket.send("Hello Server!");
+// });
+
+// // Listen for messages
+// socket.addEventListener("message", (event) => {
+//   console.log("Message from server ", event.data);
+// });
+
+
+document.getElementById('geometry-btn').addEventListener('click', async ()=>{
+    const buildingPaths =  await Forma.geometry.getPathsByCategory({category: "building"})
+    // console.log(buildingPath)
+
+    for(const path of buildingPaths )
+    {
+        const position = await Forma.geometry.getTriangles({path});
+        socket.send(position);
+    }
+})
